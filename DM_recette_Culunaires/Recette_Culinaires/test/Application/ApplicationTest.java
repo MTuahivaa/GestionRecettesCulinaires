5bdc04f77fe1273cd7932a0be6151ffd74c49fbd
jahboyLove <133338622+jahboyLove@users.noreply.github.com>
2024-12-09 22:43:53 -1000

    Modification de ApplicationTest.java

--- DM_recette_Culunaires/Recette_Culinaires/test/Application/ApplicationTest.java
+++ DM_recette_Culunaires/Recette_Culinaires/test/Application/ApplicationTest.java
@@ -12,17 +12,14 @@ class ApplicationTest {
 	@Test
 	void test() {
 		ApplicationRecetteCulinaire App = new ApplicationRecetteCulinaire();
-		App.creerUtilisateur(1, "ROCKA", "Haumana");
+		App.creerUtilisateur(1, "ROCKA", "Haumana", "RocHau");
 		App.ListeIngredientExistants = Arrays.asList(
 			new Ingredient(1, "pomme"),
 			new Ingredient(2, "Carotte"),
 			new Ingredient(3, "pomme de terre")
 		);
 		
-		App.creerUneRecette(101, "Rocka Haumana", "Tarte aux pommes", "Une délicieuse tarte aux pommes.", Arrays.asList("pomme"), App.ListeIngredientExistants);
-        App.creerUneRecette(102, "Rocka Haumana", "Soupe de carotte", "Une soupe de carotte exquise.", Arrays.asList("Carotte"), App.ListeIngredientExistants);
-        App.creerUneRecette(103, "Roger Bichard", "Boulliant de carotte", "Un bouillant de légumes.", Arrays.asList("Carotte", "pomme de terre"), App.ListeIngredientExistants);
-        
+		/*
         //recette1
         App.getListeDeToutesLesRecette().get(0).setNoteGeneral(3.4f);
         App.getListeDeToutesLesRecette().get(0).setViewCount(6);
@@ -34,20 +31,25 @@ class ApplicationTest {
         //recette3
         App.getListeDeToutesLesRecette().get(2).setNoteGeneral(3f);
         App.getListeDeToutesLesRecette().get(2).setViewCount(10);
+        */
+		
+		App.creerUneRecette(101, "Rocka", "Haumana", "Tarte aux pommes", "Une délicieuse tarte aux pommes.", Arrays.asList("pomme"), App.ListeIngredientExistants);
+        App.creerUneRecette(102, "Rocka", "Haumana", "Soupe de carotte", "Une soupe de carotte exquise.", Arrays.asList("Carotte"), App.ListeIngredientExistants);
+        App.creerUneRecette(103, "Roger", "Bichard", "Boulliant de carotte", "Un bouillant de légumes.", Arrays.asList("Carotte", "pomme de terre"), App.ListeIngredientExistants);
         
         App.RechercheParMotCles(Arrays.asList("carotte"));
         
         Recette r1 = new Recette(101, "Rocka Haumana", "Tarte aux pommes", "Une délicieuse tarte aux pommes.", Arrays.asList("pomme"), App.ListeIngredientExistants);
-        r1.setNoteGeneral(3.4f);
-        r1.setViewCount(6);
+        /*r1.setNoteGeneral(3.4f);
+        r1.setViewCount(6);*/
         
         Recette r2 = new Recette(102, "Rocka Haumana", "Soupe de carotte", "Une soupe de carotte exquise.", Arrays.asList("Carotte"), App.ListeIngredientExistants);
-        r2.setNoteGeneral(3.9f);
-        r2.setViewCount(18);
+        /*r2.setNoteGeneral(3.9f);
+        r2.setViewCount(18);*/
         
         Recette r3 = new Recette(103, "Roger Bichard", "Boulliant de carotte", "Un bouillant de légumes.", Arrays.asList("Carotte", "pomme de terre"), App.ListeIngredientExistants);
-        r3.setNoteGeneral(3f);
-        r3.setViewCount(10);
+        /*r3.setNoteGeneral(3f);
+        r3.setViewCount(10);*/
         
         List<Recette> ListeDeRecetteAttendue = new ArrayList<>();
         ListeDeRecetteAttendue.add(r2);
